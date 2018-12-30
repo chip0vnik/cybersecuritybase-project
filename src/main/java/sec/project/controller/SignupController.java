6fdc49f23fb7dc5d28b0bdd1a55fcea9270cf599
chip0vnik <46160730+chip0vnik@users.noreply.github.com>
2018-12-30 19:29:16 +0100

    Cyber security base project

--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -1,18 +1,26 @@
 package sec.project.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import sec.project.domain.Signup;
 import sec.project.repository.SignupRepository;
+import sec.project.config.CustomUserDetailsService;
 
 @Controller
 public class SignupController {
 
     @Autowired
     private SignupRepository signupRepository;
+    
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
 
     @RequestMapping("*")
     public String defaultMapping() {
@@ -25,9 +33,42 @@ public class SignupController {
     }
 
     @RequestMapping(value = "/form", method = RequestMethod.POST)
-    public String submitForm(@RequestParam String name, @RequestParam String address) {
-        signupRepository.save(new Signup(name, address));
+    public String submitForm(@RequestParam String name, @RequestParam String address, Model model) {
+        Signup signup = new Signup(name, address);
+        signupRepository.save(signup);
+        List<String> addresses = new ArrayList();
+        for (Signup a : signupRepository.findByName(name))  addresses.add(a.getAddress());
+        model.addAttribute("address", addresses);
+        model.addAttribute("name", name);
+      
+       
         return "done";
     }
 
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String loadAdmin(Model model) {
+        model.addAttribute("signups",signupRepository.findAll());
+        return "admin";
+    }
+    
+    @RequestMapping(value = "/admin_login", method = RequestMethod.GET)
+    public String loadlogin() {
+        return "admin_login";
+    }
+    
+    @RequestMapping(value = "/admin_login", method = RequestMethod.POST)
+    public String loginAdmin(@RequestParam String name, @RequestParam String password,Model model) {
+        try{
+            if (customUserDetailsService.loadUserByUsername(name)!= null) 
+            return "redirect:admin";
+            }
+        catch (UsernameNotFoundException e){
+        
+            model.addAttribute("error",e.getMessage());
+            return "error";
+        }
+        return "redirect:admin_login";
+    }    
+
+    
 }
